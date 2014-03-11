6f230890a2a835846d3268d94df77a50dc41803c
Arseniy Seroka <ars.seroka@gmail.com>
2014-03-11 19:36:25 +0000

    create map :: Day -> [Lesson]

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@ module Main where
 
 import Network.HTTP.Conduit (simpleHttp, parseUrl, urlEncodedBody, withManager, httpLbs, responseBody)
 import Prelude hiding (concat, putStrLn)
-import Data.Text (concat)
+import Data.Text
 import Data.Text.IO (putStrLn)
 import qualified Data.ByteString.Lazy as L
 import qualified Text.HTML.DOM as DOM
@@ -12,17 +12,27 @@ import Text.XML
 
 import Debug.Trace
 
-data Lesson = Lesson { startTime :: String
-                     , endTime :: String
-                     , week :: Bool
-                     , place :: String
-                     , name :: String
-                     , teacher :: String}
+data Lesson = Lesson { startTime :: Text
+                     , endTime :: Text
+                     , week :: Text
+                     , place :: Text
+                     , name :: Text
+                     , teacher :: Text} deriving Show
+lesson a b c d n x = Lesson {startTime = a, endTime = b, week = c, place = d, name = n, teacher = x} 
 
--- The data we're going to search for
+toSchedule (a:b:c:d:n:x:ss:xs) result = if a `elem` Prelude.map pack ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"] 
+    then toSchedule xs (result ++ [(a, [lesson b c d n x ss])]) 
+    else toSchedule (ss:xs) result'
+    where (curDay, lessons) = Prelude.last result
+          result' = Prelude.init result ++ [(curDay, lessons ++ [lesson a b c d n x])]
+toSchedule (a:b:c:d:n:x:[]) result = result'
+    where (curDay, lessons) = Prelude.last result
+          result' = Prelude.init result ++ [(curDay, lessons ++ [lesson a b c d n x])]
+toSchedule ([]) res = error "FUCK YOU"
+
 findNodes :: Cursor -> [Cursor]
 findNodes = element "tr" &/ checkElement (\a -> let l = elementNodes a in 
-                                let ffst = head l in
+                                let ffst = Prelude.head l in
                                     case ffst of
                                         NodeElement a' -> elementName a' /= "strong"
                                         NodeContent _ -> True
@@ -34,13 +44,14 @@ cursorFor page = do
 
 main :: IO ()
 main = do
-  initReq <- parseUrl "http://www.ifmo.ru/module/isu_schedule.php"
-  let req' = initReq
-  let req = (flip urlEncodedBody) req' $
-             [ ("group", "3538"),
-               ("week", "0") ]
-  response <- withManager $ httpLbs req
-  let page = responseBody response
-  {-L.putStrLn page-}
-  cursor <- cursorFor page
-  mapM_ putStrLn $ cursor $// findNodes &// content
+    initReq <- parseUrl "http://www.ifmo.ru/module/isu_schedule.php"
+    let req' = initReq
+    let req = (flip urlEncodedBody) req' $
+                [ ("group", "3538"),
+                ("week", "0") ]
+    response <- withManager $ httpLbs req
+    let page = responseBody response
+    cursor <- cursorFor page
+    let res = Prelude.filter (/= pack ": лек+пр.") $ cursor $// findNodes &// content
+    let a = toSchedule res []
+    mapM_ (putStrLn . pack . show) a
